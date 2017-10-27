6f0320469c223e9ad5cdb202581ee6ad7b1b4205
Rolf office <r.harkes@nki.nl>
2017-10-27 14:49:31 +0200

    Can now take sqrt of all pixels. (Small steps)

--- src/main/java/TemporalMedian.java
+++ src/main/java/TemporalMedian.java
@@ -6,91 +6,201 @@
  *     http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-import io.scif.services.DatasetIOService;
-
-import java.io.File;
-import java.io.IOException;
-
+import ij.ImagePlus;
+import ij.ImageStack;
 import net.imagej.Dataset;
 import net.imagej.DatasetService;
 import net.imagej.ImageJ;
+import net.imagej.ImgPlus;
+import net.imagej.axis.Axes;
 import net.imagej.axis.AxisType;
-import net.imagej.ops.OpService;
-import net.imglib2.Cursor;
-import net.imglib2.IterableInterval;
-import net.imglib2.RandomAccess;
-import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.real.FloatType;
-
+import net.imagej.axis.CalibratedAxis;
+import net.imagej.ops.Op;
+import net.imagej.ops.OpEnvironment;
+import net.imglib2.img.ImagePlusAdapter;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import org.scijava.AbstractContextual;
 import org.scijava.ItemIO;
-import org.scijava.ItemVisibility;
-import org.scijava.app.StatusService;
-import org.scijava.command.Command;
-import org.scijava.command.Previewable;
-import org.scijava.log.LogService;
+import org.scijava.convert.ConvertService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-import org.scijava.widget.FileWidget;
 
-@Plugin(type = Command.class, headless = true,
-        menuPath = "NKI>Temporal Median Filter")
-public class TemporalMedian implements Command, Previewable {
+import java.util.stream.IntStream;
+
+/**
+ * An example class to test wrapping and unwrapping of a ImageJ2 Dataset. An Op
+ * which inverts the colors of the given image.
+ *
+ * Internally the plugin operates with IJ1 style ImagePlus, but externally it
+ * operates with IJ2 Datasets
+ *
+ * How to run via the Java API: 1) Create an instance of the class 2) Call
+ * setContext with e.g. ImageJ#getContext() 3) Call setDataset 4) Call run 5)
+ * Call getDataset to get the result
+ *
+ * How to run via an opService e.g. ImageJ.op() 1) Create an instance of ImageJ
+ * 2) Call final Object outputDataset = ij.op().run("datasetExample",
+ * inputDataset) 3) Object outputDataset contains the resulting dataset
+ *
+ * The class AbstractContextual is inherited so that you can run this class via
+ * Java API. When you run it via an opService the service fields are populated
+ * automatically.
+ *
+ * @author Richard Domander
+ */
+@Plugin(type = Op.class, name = "datasetExample", menuPath = "NKI>Temporal Median")
+public class TemporalMedian extends AbstractContextual implements Op {
+    private ImagePlus image = null;
 
     @Parameter
-    private LogService log;
+    private ConvertService convertService = null;
 
     @Parameter
-    private StatusService statusService;
+    private DatasetService datasetService = null;
 
-    @Parameter
-    private DatasetService datasetService;
+    @Parameter(type = ItemIO.BOTH)
+    private Dataset dataset = null;
 
-    @Parameter
-    private DatasetIOService datasetIOService;
+    public Dataset getDataset() {
+        return dataset;
+    }
 
-    @Parameter(visibility = ItemVisibility.MESSAGE)
-    private final String header = "Apply temporal median filter";
+    public void setDataset(Dataset dataset) {
+        checkDataset(dataset);
+        this.dataset = dataset;
+        setImagePlus();
+    }
 
-    @Parameter(label = "Input 1")
-    private File file1;
+    @Override
+    public OpEnvironment ops() {
+        return null;
+    }
 
-    @Parameter(label = "Result 1", type = ItemIO.OUTPUT)
-    private Dataset result1;
-
-    public static void main(final String... args) throws Exception {
-        // create the ImageJ application context with all available services
-        final ImageJ ij = net.imagej.Main.launch(args);
-
-        ij.command().run(TemporalMedian.class, true);
+    @Override
+    public void setEnvironment(OpEnvironment opEnvironment) {
     }
 
     @Override
     public void run() {
-        Dataset input1;
-        try {
-            input1 = datasetIOService.open(file1.getAbsolutePath());
-        } catch (final IOException e) {
-            log.error(e);
-            return;
+        checkNotNull(convertService, "Missing services - did you remember to call setContext?");
+        checkNotNull(datasetService, "Missing services - did you remember to call setContext?");
+
+        if (image == null) {
+            // the plugin is (probably) run from an opService, check the dataset
+            // obtained as @Parameter
+            setDataset(dataset);
         }
 
-        result1 = CalcTMF(input1);
+        makeNegative();
+
+        ImgPlus<UnsignedShortType> imgPlus = ImagePlusAdapter.wrapImgPlus(image);
+        dataset = datasetService.create(imgPlus);
     }
 
-    @Override
-    public void cancel() {
-        log.info("Cancelled");
+    // region -- Utility methods --
+
+    /** Run the plugin via Java API */
+    public static void main(final String... args) throws Exception {
+        int width = 320;
+        int height = 200;
+        int depth = 10;
+
+        // Create an instance of ImageJ
+        final ImageJ ij = new ImageJ();
+
+        // NB if you show the UI after setting the Dataset, an the program
+        // automatically shows the image
+        // This side effect has been reported
+        ij.ui().showUI();
+
+        // Create a white monochrome dataset for the Plugin
+        final Dataset inputDataset = createMonochromeDataset(ij, width, height, depth, 0xFFFF);
+
+        final TemporalMedian datasetExample = new TemporalMedian();
+
+        // Set the context for the plugin to populate service @Parameters
+        datasetExample.setContext(ij.getContext());
+
+        try {
+            datasetExample.setDataset(inputDataset);
+            datasetExample.run();
+        } catch (NullPointerException | IllegalArgumentException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        // Display the image the plugin produced
+        Dataset outputDataset = datasetExample.getDataset();
+        ij.ui().show(outputDataset);
     }
 
-    @Override
-    public void preview() {
-        log.info("previews TemporalMedian");
-        statusService.showStatus(header);
-    }    
-    
-    @SuppressWarnings({"rawtypes"})
-    private Dataset CalcTMF(final Dataset d1) {
-        return d1;
+    public static Dataset createMonochromeDataset(final ImageJ ijInstance, final int width, final int height,
+                                                  final int depth, final int color) {
+        final long[] dims = {width, height, depth};
+        final AxisType[] axisTypes = {Axes.X, Axes.Y, Axes.TIME};
+        Dataset dataset = ijInstance.dataset().create(new UnsignedShortType(), dims, "Test image", axisTypes);
+
+        IntStream.range(0, depth).forEach(i -> {
+            final short[] data = new short[width * height];
+            IntStream.range(0, data.length).forEach(j -> data[j] = (short) color);
+            dataset.setPlane(i, data);
+        });
+
+        return dataset;
     }
+    // endregion
+
+    // region -- Helper methods --
+    private void checkNotNull(final Object object, final String message) {
+        if (object == null) {
+            throw new NullPointerException(message);
+        }
+    }
+
+    private void checkArgument(final boolean argument, final String message) {
+        if (!argument) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    /**
+     * Check whether the Op can operate with the given dataset
+     *
+     * @throws NullPointerException     if dataset == null
+     * @throws IllegalArgumentException if the dataset has wrong number of dimensions
+     * @throws IllegalArgumentException if the dataset cannot be converted into an ImagePlus
+     */
+    private void checkDataset(final Dataset dataset) {
+        checkNotNull(dataset, "Dataset cannot be null");
+        checkDatasetDimensions(dataset);
+        checkArgument(convertService.supports(dataset, ImagePlus.class), "Cannot convert given dataset");
+    }
+
+    private void checkDatasetDimensions(final Dataset dataset) {
+        checkArgument(dataset.numDimensions() == 3, "The plugin is meant only for 3D images");
+        CalibratedAxis axes[] = new CalibratedAxis[3];
+        dataset.axes(axes);
+        checkArgument(axes[0].type().isSpatial(), "Unexpected 1st dimension");
+        checkArgument(axes[1].type().isSpatial(), "Unexpected 2nd dimension");
+        checkArgument(axes[2].type().equals(Axes.TIME), "Unexpected 3rd dimension");
+    }
+
+    /**
+     * Inverts the value of each bit in each pixel
+     */
+    private void makeNegative() {
+        final int depth = image.getNFrames();
+        final ImageStack stack = image.getStack();
+
+        IntStream.rangeClosed(1, depth).parallel().forEach(t -> {
+            short pixels[] = (short[]) stack.getPixels(t);
+            for (int i = 0; i < pixels.length; i++) {
+                pixels[i] = (short) Math.sqrt((double) pixels[i] );
+            }
+        });
+    }
+
+    private void setImagePlus() {
+        image = convertService.convert(dataset, ImagePlus.class);
+    }
+    // endregion
 }
