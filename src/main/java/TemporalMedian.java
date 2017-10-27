7e34f2ca87d5bb86a05344fa93d5622f2df248a5
Rolf office <r.harkes@nki.nl>
2017-10-27 15:08:05 +0200

    Cleanup of code. Possibly broke the backwards compatibility with ImageJ 1.

--- src/main/java/TemporalMedian.java
+++ src/main/java/TemporalMedian.java
@@ -1,24 +1,15 @@
 /*
- * To the extent possible under law, the ImageJ developers have waived
- * all copyright and related or neighboring rights to this tutorial code.
- *
- * See the CC0 1.0 Universal license for details:
- *     http://creativecommons.org/publicdomain/zero/1.0/
+ * 
  */
 
 import ij.ImagePlus;
 import ij.ImageStack;
 import net.imagej.Dataset;
 import net.imagej.DatasetService;
-import net.imagej.ImageJ;
-import net.imagej.ImgPlus;
 import net.imagej.axis.Axes;
-import net.imagej.axis.AxisType;
 import net.imagej.axis.CalibratedAxis;
 import net.imagej.ops.Op;
 import net.imagej.ops.OpEnvironment;
-import net.imglib2.img.ImagePlusAdapter;
-import net.imglib2.type.numeric.integer.UnsignedShortType;
 import org.scijava.AbstractContextual;
 import org.scijava.ItemIO;
 import org.scijava.convert.ConvertService;
@@ -28,25 +19,9 @@ import org.scijava.plugin.Plugin;
 import java.util.stream.IntStream;
 
 /**
- * An example class to test wrapping and unwrapping of a ImageJ2 Dataset. An Op
- * which inverts the colors of the given image.
+ * A plugin to calculate the Temporal Median of a dataset
  *
- * Internally the plugin operates with IJ1 style ImagePlus, but externally it
- * operates with IJ2 Datasets
- *
- * How to run via the Java API: 1) Create an instance of the class 2) Call
- * setContext with e.g. ImageJ#getContext() 3) Call setDataset 4) Call run 5)
- * Call getDataset to get the result
- *
- * How to run via an opService e.g. ImageJ.op() 1) Create an instance of ImageJ
- * 2) Call final Object outputDataset = ij.op().run("datasetExample",
- * inputDataset) 3) Object outputDataset contains the resulting dataset
- *
- * The class AbstractContextual is inherited so that you can run this class via
- * Java API. When you run it via an opService the service fields are populated
- * automatically.
- *
- * @author Richard Domander
+ * @author R.Harkes & B.van den Broek
  */
 @Plugin(type = Op.class, name = "datasetExample", menuPath = "NKI>Temporal Median")
 public class TemporalMedian extends AbstractContextual implements Op {
@@ -91,65 +66,10 @@ public class TemporalMedian extends AbstractContextual implements Op {
             setDataset(dataset);
         }
 
-        makeNegative();
-
-        ImgPlus<UnsignedShortType> imgPlus = ImagePlusAdapter.wrapImgPlus(image);
-        dataset = datasetService.create(imgPlus);
+        makeNegative(); //the main calculation. A void function that manipulates image.
     }
 
-    // region -- Utility methods --
-
-    /** Run the plugin via Java API */
-    public static void main(final String... args) throws Exception {
-        int width = 320;
-        int height = 200;
-        int depth = 10;
-
-        // Create an instance of ImageJ
-        final ImageJ ij = new ImageJ();
-
-        // NB if you show the UI after setting the Dataset, an the program
-        // automatically shows the image
-        // This side effect has been reported
-        ij.ui().showUI();
-
-        // Create a white monochrome dataset for the Plugin
-        final Dataset inputDataset = createMonochromeDataset(ij, width, height, depth, 0xFFFF);
-
-        final TemporalMedian datasetExample = new TemporalMedian();
-
-        // Set the context for the plugin to populate service @Parameters
-        datasetExample.setContext(ij.getContext());
-
-        try {
-            datasetExample.setDataset(inputDataset);
-            datasetExample.run();
-        } catch (NullPointerException | IllegalArgumentException e) {
-            System.out.println("Exception: " + e.getMessage());
-        }
-
-        // Display the image the plugin produced
-        Dataset outputDataset = datasetExample.getDataset();
-        ij.ui().show(outputDataset);
-    }
-
-    public static Dataset createMonochromeDataset(final ImageJ ijInstance, final int width, final int height,
-                                                  final int depth, final int color) {
-        final long[] dims = {width, height, depth};
-        final AxisType[] axisTypes = {Axes.X, Axes.Y, Axes.TIME};
-        Dataset dataset = ijInstance.dataset().create(new UnsignedShortType(), dims, "Test image", axisTypes);
-
-        IntStream.range(0, depth).forEach(i -> {
-            final short[] data = new short[width * height];
-            IntStream.range(0, data.length).forEach(j -> data[j] = (short) color);
-            dataset.setPlane(i, data);
-        });
-
-        return dataset;
-    }
-    // endregion
-
-    // region -- Helper methods --
+    // Helper methods --
     private void checkNotNull(final Object object, final String message) {
         if (object == null) {
             throw new NullPointerException(message);
@@ -185,7 +105,7 @@ public class TemporalMedian extends AbstractContextual implements Op {
     }
 
     /**
-     * Inverts the value of each bit in each pixel
+     * Main calculation loop. Manipulates the data in image.
      */
     private void makeNegative() {
         final int depth = image.getNFrames();
