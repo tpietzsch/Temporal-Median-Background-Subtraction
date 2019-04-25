530b616574833f818fd8244996aa2cd907676273
Rolf office <r.harkes@nki.nl>
2019-04-25 16:50:22 +0200

    Changed to ImgLib2, now 50% slower.

--- src/main/java/nl/nkiavl/rharkes/TemporalMedian.java
+++ src/main/java/nl/nkiavl/rharkes/TemporalMedian.java
@@ -1,3 +1,4 @@
+package nl.nkiavl.rharkes;
 /* Fast Temporal Median filter 
 (c) 2017 Rolf Harkes and Bram van den Broek, Netherlands Cancer Institute.
 Based on the Fast Temporal Median Filter for ImageJ by the Milstein Lab.
@@ -24,12 +25,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 
-import ij.IJ;
-import ij.ImagePlus;
-import ij.ImageStack;
-import ij.process.ImageConverter;
-import ij.process.StackConverter;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.Views;
 
 import org.scijava.app.StatusService;
 import org.scijava.command.Command;
@@ -52,74 +53,69 @@ public class TemporalMedian implements Command, Previewable {
     private StatusService statusService;
 
     @Parameter(label = "Select image", description = "the image field")
-    private ImagePlus image1;
+    private Img<UnsignedShortType> img;
 
     @Parameter(label = "Median window", description = "the frames for medan")
-    private short window;
+    private int window;
 
     @Parameter(label = "Added offset", description = "offset added to the new image")
     private short offset;
 
-    //@Parameter(label = "Ignore this", description = "Do not use this field", visibility = ItemVisibility.INVISIBLE)
-    //private ImagePlus IGNOREimage; //never used, need for getting a list selection for image1
     public static void main(final String... args) throws Exception {
 
     }
 
     @Override
     public void run() {
-        double bitdepth = (double) image1.getBitDepth(); //declare double for raising to power
+    	//check input image
+    	UnsignedShortType tempPixel = img.firstElement();
+    	int bitdepth = tempPixel.getBitsPerPixel();
+    	int values = 1 << bitdepth;
         if (bitdepth != 16) {
-            log.warn("BitDepth must 16 but was " + image1.getBitDepth() + ". Will convert now");
-            if (ImageConverter.getDoScaling()) {
-                ImageConverter.setDoScaling(false);
-                new StackConverter(image1).convertToGray16();
-                ImageConverter.setDoScaling(true);
-            } else {
-                new StackConverter(image1).convertToGray16();
-            }
+            log.error("BitDepth must 16 but was " + bitdepth);
+            return;
         }
-        //get datastack
-        image1.deleteRoi(); //remove to prevent a partial duplicate
-        ImageStack stack1 = image1.getStack();
-        int w = stack1.getWidth();
-        int h = stack1.getHeight();
-        int t = stack1.getSize();
-        int pixels = w * h;
+		final long[] dims = new long[img.numDimensions()];
+		img.dimensions(dims);
+		final int pixels = (int)Intervals.numElements(img);
+		final int pixelsPerFrame = (int)dims[0] * (int)dims[1];
+
+		log.info("nr of pixels = "+pixels);
+		log.info("nr of pixels per frame = "+pixelsPerFrame);
+		log.info("nr of bits per pixel = "+bitdepth);
+		img.dimensions(dims);
+		if ((dims[0]*dims[1]*dims[2]) > Math.pow(2, 32)){
+            log.error("No support for more than 4.294.967.296 pixels. Please concider splitting the image.");return;
+        }
+		
         //sanity check on the input
-        if (window >= (short) t) {
-            window = (short) t;
+        if (window >= (int) dims[2]) {
+            window = (int) dims[2];
             if (window % 2 == 0) {window--;}
             log.warn("Window is larger than largest dimension. Reducing window to " + window);
         } else if (window % 2 == 0) {
             window++;
             log.warn("No support for even windows. Window = " + window);
         }
-        if (((long) t * (long) pixels)>(2^32)){
-            log.error("No support for more than 4.294.967.296 pixels. Please concider splitting the image.");return;
-        }
-        //allocate data storage
-        log.debug("allocating datastorage");
-        short data[] = new short[t * pixels];
-        short temp[] = new short[pixels];
-        log.debug("finished");
-        log.debug("loading data from stack");
-        boolean inihist[] = new boolean[65536];
-        statusService.showStatus("loading data");
-        Object[] imagearray = stack1.getImageArray();
-        for (int i = 0; i < t; i++) { //all timepoints
-            statusService.showProgress(i, t);
-            temp = (short[]) imagearray[i]; 
-            for (int p = 0; p < pixels; p++) {//all pixels
-                data[i + p * t] = temp[p];
-                inihist[temp[p]] = true;
-            }
-        }
-        log.debug("finished");
-        log.debug("compress data");
+        
+		//Allocate data storage
+		statusService.showStatus("allocating datastorage");      
+		final short data[] = new short[pixels];
+		//Load data
+		statusService.showStatus("loading data");
+		int p = 0;
+		short temp =0;
+        boolean inihist[] = new boolean[values];
+		for (UnsignedShortType s : Views.flatIterable(img)) {
+			temp = s.getShort();
+			inihist[temp] = true;
+			data[p++] = temp; 
+			if ((p % 2000) == 0) {
+				statusService.showProgress(p, pixels);
+			}
+		}
         //compress data
-        int addindex[] = compress(data, inihist);
-        log.debug("finished");
+        int unrankArray[] = denseRank(data, inihist);
         //do calculation in parallel per pixel
         final AtomicInteger ai = new AtomicInteger(0); //special unqique int for each thread
         final Thread[] threads = newThreadArray(); //all threads
@@ -129,10 +125,10 @@ public class TemporalMedian implements Command, Previewable {
                     setPriority(Thread.NORM_PRIORITY);
                 }
                 public void run() {
-                    for (int i = ai.getAndIncrement(); i < pixels; i = ai.getAndIncrement()) { //get unique i
-                        substrmedian(data, i, t, addindex,window,offset);
+                    for (int i = ai.getAndIncrement(); i < pixelsPerFrame; i = ai.getAndIncrement()) { //get unique i
+                        substrmedian(data, i, pixelsPerFrame, dims, unrankArray,window,offset);
                         if ((i%200)==0){
-                            statusService.showProgress(i, pixels);
+                            statusService.showProgress(i, pixelsPerFrame);
                         }
                     }
                 }
@@ -141,22 +137,18 @@ public class TemporalMedian implements Command, Previewable {
         statusService.showStatus("calculating median");
         startAndJoin(threads);
         //make stack and count zeros
-        int zeros = 0;
-        statusService.showStatus("making image");
-        for (int i = 0; i < t; i++) { //all timepoints
-            statusService.showProgress(i, t);
-            for (int p = 0; p < pixels; p++) { //all pixels
-                temp[p] = data[i + p * t];
-                if (temp[p]<0){zeros++;temp[p]=0;}
-            }
-            stack1.setPixels(temp.clone(), (i+1));
-        }
-        if (zeros>0&&(zeros/t)<10){log.info(zeros + " pixels went below zero. Concider increasing the offset." + (zeros/t) + " pixels per frame.");}
-        if ((zeros/t)>=10){log.warn(zeros + " pixels went below zero. Concider increasing the offset." + (zeros/t) + " pixels per frame.");}
-        image1.setStack(stack1);
-        image1.setTitle("MEDFILT_" + image1.getTitle());
-        image1.setDisplayMode(IJ.GRAYSCALE);
-        image1.show();
+        statusService.showStatus("return data to image median");
+        int zeros = 0;p=0;
+        for (UnsignedShortType s : Views.flatIterable(img)) {
+			temp = data[p++] ;
+			if (temp<0){zeros++;temp=0;}
+			s.setShort(temp);
+			if ((p % 2000) == 0) {
+				statusService.showProgress(p, pixels);
+			}
+		}
+        if (zeros>0&&(zeros/dims[2])<10){log.info(zeros + " pixels went below zero. Concider increasing the offset." + (zeros/dims[2]) + " pixels per frame.");}
+        if ((zeros/dims[2])>=10){log.warn(zeros + " pixels went below zero. Concider increasing the offset." + (zeros/dims[2]) + " pixels per frame.");}
         statusService.showStatus(1, 1, "FINISHED");
     }
 
@@ -201,29 +193,29 @@ public class TemporalMedian implements Command, Previewable {
      * @param inihist boolean array of length 65536 that is true if the value exists
      * @return        decompression array that can reverse the compression step
      */
-    public static int[] compress(short[] data, boolean[] inihist) {
+    public static int[] denseRank(short[] data, boolean[] inihist) {
         int values = (int) 65536;
         int subtract[] = new int[values];
-        int decompressF[] = new int[values]; //decompress array decompress[data] --> original data
+        int unrankArrayF[] = new int[values]; //decompress array decompress[data] --> original data
         int idx = 0;
         int subtractvalue = 0;
         for (int i = 0; i < values; i++) {
             if (inihist[i]) {
                 subtract[i] = subtractvalue;
-                decompressF[idx] = subtractvalue + idx;
+                unrankArrayF[idx] = subtractvalue + idx;
                 idx++;
             } else {
                 subtractvalue++;
             }
         }
-        //trim the decompress array
-        int decompress[] = new int[idx];
-        System.arraycopy(decompressF, 0, decompress, 0, idx);
+        //trim the unrank array
+        int unrankArray[] = new int[idx];
+        System.arraycopy(unrankArrayF, 0, unrankArray, 0, idx);
         //compress data
         for (int i = 0; i < data.length; i++) {
             data[i] -= subtract[data[i]];
         }
-        return decompress;
+        return unrankArray;
     }
 
     /**
@@ -233,18 +225,18 @@ public class TemporalMedian implements Command, Previewable {
      *
      * @param data     ranked data with values from 0 to addindex.length
      * @param pix      pixel to analyse
-     * @param T        number of timepoints
+     * @param dims     dimensions of the image data
      * @param addindex decompression array to go from ranked to original data
      * @param window   temporal window (must be odd)
      * @param offset   added to the each pixel before subtraction to prevent underflow
      */
-    public void substrmedian(short[] data, int pix, int T, int[] addindex, short window, short offset) {
+    public void substrmedian(short[] data, int pix, int pixelsPerFrame, long[] dims, int[] unrankArray, int window, short offset) {
+    	int T = (int) dims[2]; 
         int windowC = (window - 1) / 2; //0 indexed sorted array has median at this position.
         int tempres[] = new int[T-window+1]; //store decompressed medians for subtraction after the loop
-        int step = pix * T;
         short pixel;
         short pixel2;
-        short hist[] = new short[addindex.length]; //Gray-level histogram init at 0
+        short hist[] = new short[unrankArray.length]; //Gray-level histogram init at 0
         short median = 0;//The median of this pixel
         short aux = 0;   //Marks the position of the median pixel in the column of the histogram, starting with 1
         for (int t = 0; t <= (T - window); t++) //over all timepoints
@@ -253,7 +245,7 @@ public class TemporalMedian implements Command, Previewable {
             {
                 for (int t2 = 0; t2 < window; t2++) //For each frame inside the window
                 {
-                    hist[data[step + t2]]++; //Add it to the histogram
+                    hist[data[pix + pixelsPerFrame*t2]]++; //Add it to the histogram
                 }
                 short count = 0, j = -1;
                 while (count <= windowC) //Counting the histogram, until it reaches the median
@@ -264,8 +256,8 @@ public class TemporalMedian implements Command, Previewable {
                 aux = (short) (count - (int) windowC); //position in the bin. 1 is lowest.
                 median = j;
             } else {
-                pixel = data[t + step - 1]; //Old pixel remove from the histogram
-                pixel2 = data[t + window + step-1]; //New pixel, add to the histogram
+                pixel = data[pix + pixelsPerFrame*(t - 1)]; //Old pixel remove from the histogram
+                pixel2 = data[pix + pixelsPerFrame*(t + window-1)]; //New pixel, add to the histogram
                 hist[pixel]--; //Removing old pixel
                 hist[pixel2]++; //Adding new pixel
                 if (!(((pixel > median)
@@ -343,17 +335,17 @@ public class TemporalMedian implements Command, Previewable {
                     }
                 }
             }
-            tempres[t] = addindex[median];
+            tempres[t] = unrankArray[median];
         }
         // now convert this pixel back from rank to original data and subtract the median
         // this must be done AFTER median calculation. Otherwise we mix rank and original.
         for (int t = 0; t < T; t++) {
             if (t <= windowC) {            //Apply first median to frame 0->windowC
-                data[t+step] = (short) (offset+addindex[data[t+step]] - tempres[0]);
+                data[pix + pixelsPerFrame*t] = (short) (offset+unrankArray[data[pix + pixelsPerFrame*t]] - tempres[0]);
             } else if (t<(T - windowC)) {  //Apply median from windowC back to the current frame 
-                data[t+step] = (short) (offset+addindex[data[t+step]] - tempres[t-windowC]);
+                data[pix + pixelsPerFrame*t] = (short) (offset+unrankArray[data[pix + pixelsPerFrame*t]] - tempres[t-windowC]);
             } else {                      //Apply last median to frame (T-windowC)->T
-                data[t+step] = (short) (offset+addindex[data[t+step]] - tempres[tempres.length-1]);
+                data[pix + pixelsPerFrame*t] = (short) (offset+unrankArray[data[pix + pixelsPerFrame*t]] - tempres[tempres.length-1]);
             }
         }
     }
