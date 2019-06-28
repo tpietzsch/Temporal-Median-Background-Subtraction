4c7afe199c984f44259abbbe9bdd17809973e1f2
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2019-06-28 11:27:58 +0200

    single-threaded seems to be working?

--- src/main/java/imglib/Playground.java
+++ src/main/java/imglib/Playground.java
@@ -1,11 +1,15 @@
 package imglib;
 
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
 import net.imglib2.IterableInterval;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.converter.Converters;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 
 import java.util.Arrays;
@@ -193,55 +197,63 @@ public class Playground {
     }
 
     public static void main(String[] args) {
-//        new ImageJ();
-//        final ImagePlus imp = IJ.openImage("/Users/pietzsch/Desktop/random-smalltif");
-//        final Img<UnsignedShortType> img = ImageJFunctions.wrap(imp);
-//
-//        final RankMap rankmap = RankMap.build(img);
-//        final RandomAccessibleInterval<UnsignedShortType> ranked = Converters.convert((RandomAccessibleInterval) img, rankmap::toRanked, new UnsignedShortType());
-//        ImageJFunctions.show(ranked);
-//
-//        final int window = 9;
-//        final int windowC = (window - 1) / 2; //0 indexed sorted array has median at this position.
-//
-//        for (int y = 0; y < img.dimension(1); ++y) {
-//            for (int x = 0; x < img.dimension(0); ++x) {
-//                RandomAccess<UnsignedShortType> front = ranked.randomAccess();
-//                front.setPosition(new int[]{x, y, 0});
-//
-//                RandomAccess<UnsignedShortType> back = img.randomAccess();
-//                back.setPosition(new int[]{x, y, 0});
-//
-//                MedianHistogram filter = new MedianHistogram(window, rankmap.getMaxRank());
-//
-//                // read the first window ranked pixels into median filter
-//                for (int i = 0; i < window; ++i) {
-//                    filter.add((short) front.get().get());
-//                    front.fwd(2);
-//                }
-//                // write current median for windowC+1 pixels
-//                for (int i = 0; i <= windowC; ++i) {
-//                    back.get().set(filter.get());
-//                    back.fwd(2);
-//                }
-//
-//
-//                final int zSize = (int)img.dimension(2);
-//                final int zSteps = zSize - window;
-//                for (int i = 0; i < zSteps; ++i) {
-//                    filter.add((short) front.get().get());
-//                    front.fwd(2);
-//                    back.get().set(filter.get());
-//                    back.fwd(2);
-//                }
-//
-//                // write current median for windowC pixels
-//                for (int i = 0; i < windowC; ++i) {
-//                    back.get().set(filter.get());
-//                    back.fwd(2);
-//                }
-//            }
-//        }
-        testMedianExt();
+        new ImageJ();
+        final ImagePlus imp = IJ.openImage("/Users/pietzsch/Desktop/random-small.tif");
+        imp.show();
+        final Img<UnsignedShortType> img = ImageJFunctions.wrap(imp);
+
+        System.out.println("starting ... ");
+        final long t0 = System.currentTimeMillis();
+
+        final RankMap rankmap = RankMap.build(img);
+        final RandomAccessibleInterval<UnsignedShortType> ranked = Converters.convert((RandomAccessibleInterval) img, rankmap::toRanked, new UnsignedShortType());
+        ImageJFunctions.show(ranked, "ranked");
+
+        final int window = 501;
+        final int offset = 3000;
+        final int windowC = (window - 1) / 2; //0 indexed sorted array has median at this position.
+
+        RandomAccess<UnsignedShortType> front = ranked.randomAccess();
+        RandomAccess<UnsignedShortType> back = img.randomAccess();
+        MedianHistogram filter = new MedianHistogram(window, rankmap.getMaxRank());
+        for (int y = 0; y < img.dimension(1); ++y) {
+            for (int x = 0; x < img.dimension(0); ++x) {
+                front.setPosition(new int[]{x, y, 0});
+                back.setPosition(new int[]{x, y, 0});
+
+                // read the first window ranked pixels into median filter
+                for (int i = 0; i < window; ++i) {
+                    filter.add((short) front.get().get());
+                    front.fwd(2);
+                }
+                // write current median for windowC+1 pixels
+                for (int i = 0; i <= windowC; ++i) {
+                    final UnsignedShortType t = back.get();
+                    t.set(t.get() + offset - rankmap.fromRanked(filter.get()));
+                    back.fwd(2);
+                }
+
+                final int zSize = (int)img.dimension(2);
+                final int zSteps = zSize - window;
+                for (int i = 0; i < zSteps; ++i) {
+                    filter.add((short) front.get().get());
+                    front.fwd(2);
+                    final UnsignedShortType t = back.get();
+                    t.set(t.get() + offset - rankmap.fromRanked(filter.get()));
+                    back.fwd(2);
+                }
+
+                // write current median for windowC pixels
+                for (int i = 0; i < windowC; ++i) {
+                    final UnsignedShortType t = back.get();
+                    t.set(t.get() + offset - rankmap.fromRanked(filter.get()));
+                    back.fwd(2);
+                }
+            }
+        }
+
+        final long t1 = System.currentTimeMillis();
+        System.out.println("(t1-t0) = " + (t1 - t0));
+        System.out.println("done");
     }
 }
