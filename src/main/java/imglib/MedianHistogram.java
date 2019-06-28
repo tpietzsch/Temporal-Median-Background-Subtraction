48af7ae9a53b7fb7a49d0cf845fab8c852e0d5ae
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2019-06-28 11:55:50 +0200

    Just initialize histogram and aux as if window 0's have been observed. This simplifies MEdianHistogram.add()

--- src/main/java/imglib/MedianHistogram.java
+++ src/main/java/imglib/MedianHistogram.java
@@ -7,9 +7,8 @@ public class MedianHistogram {
     private final int windowC; //0 indexed sorted array has median at this position.
     private final short[] hist; //Gray-level histogram init at 0
 
-    private int t = 0;
-    private short median = -1;//The median of this pixel
-    private short aux = 0;   //Marks the position of the median pixel in the column of the histogram, starting with 1
+    private short median;//The median of this pixel
+    private short aux;   //Marks the position of the median pixel in the column of the histogram, starting with 1
 
     /**
      * @param window window width of the median filter
@@ -21,6 +20,10 @@ public class MedianHistogram {
 
         windowC = (window - 1) / 2; //0 indexed sorted array has median at this position.
         hist = new short[maxVal + 1]; //Gray-level histogram init at 0
+
+        hist[0] = (short) window;
+        aux = (short) (windowC + 1);
+        median = 0;
 
         history = new short[window];
         hi = 0;
@@ -40,32 +43,54 @@ public class MedianHistogram {
     public void add(final short pixel2) {
         final short pixel = record(pixel2);
 
-        if (t < window) //Building the first histogram
+        hist[pixel]--; //Removing old pixel
+        hist[pixel2]++; //Adding new pixel
+        if (!(
+                (pixel > median && pixel2 > median)
+                        || (pixel < median && pixel2 < median)
+                        || (pixel == median && pixel2 == median)
+        )) //Add and remove the same pixel, or pixel from the same side, the median doesn't change
         {
-            ++hist[pixel2];
-            if (++t == window) {
-                int count = 0, j = -1;
-                while (count <= windowC) //Counting the histogram, until it reaches the median
+            int j = median;
+            if ((pixel2 > median) && (pixel < median)) //The median goes right
+            {
+                if (hist[median] == aux) //The previous median was the last pixel of its column in the histogram, so it changes
                 {
                     j++;
-                    count += hist[j];
+                    while (hist[j] == 0) //Searching for the next pixel
+                    {
+                        j++;
+                    }
+                    median = (short) (j);
+                    aux = 1; //The median is the first pixel of its column
+                } else {
+                    aux++; //The previous median wasn't the last pixel of its column, so it doesn't change, just need to mark its new position
                 }
-                aux = (short) (windowC + hist[j] + 1 - count); //position in the bin. 1 is lowest.
-                median = (short) j;
-            }
-        } else {
-            hist[pixel]--; //Removing old pixel
-            hist[pixel2]++; //Adding new pixel
-            if (!(
-                    (pixel > median && pixel2 > median)
-                            || (pixel < median && pixel2 < median)
-                            || (pixel == median && pixel2 == median)
-            )) //Add and remove the same pixel, or pixel from the same side, the median doesn't change
+            } else if ((pixel > median) && (pixel2 < median)) //The median goes left
             {
-                int j = median;
-                if ((pixel2 > median) && (pixel < median)) //The median goes right
+                if (aux == 1) //The previous median was the first pixel of its column in the histogram, so it changes
                 {
-                    if (hist[median] == aux) //The previous median was the last pixel of its column in the histogram, so it changes
+                    j--;
+                    while (hist[j] == 0) //Searching for the next pixel
+                    {
+                        j--;
+                    }
+                    median = (short) (j);
+                    aux = hist[j]; //The median is the last pixel of its column
+                } else {
+                    aux--; //The previous median wasn't the first pixel of its column, so it doesn't change, just need to mark its new position
+                }
+            } else if (pixel2 == median) //new pixel = last median
+            {
+                if (pixel < median) //old pixel < last median, the median goes right
+                {
+                    aux++; //There is at least one pixel above the last median (the one that was just added), so the median doesn't change, just need to mark its new position
+                }                                //else, absolutely nothing changes
+            } else //pixel==median, old pixel = last median
+            {
+                if (pixel2 > median) //new pixel > last median, the median goes right
+                {
+                    if (aux == (hist[median] + 1)) //The previous median was the last pixel of its column, so it changes
                     {
                         j++;
                         while (hist[j] == 0) //Searching for the next pixel
@@ -74,10 +99,9 @@ public class MedianHistogram {
                         }
                         median = (short) (j);
                         aux = 1; //The median is the first pixel of its column
-                    } else {
-                        aux++; //The previous median wasn't the last pixel of its column, so it doesn't change, just need to mark its new position
                     }
-                } else if ((pixel > median) && (pixel2 < median)) //The median goes left
+                    //else, absolutely nothing changes
+                } else //pixel2<median, new pixel < last median, the median goes left
                 {
                     if (aux == 1) //The previous median was the first pixel of its column in the histogram, so it changes
                     {
@@ -90,42 +114,6 @@ public class MedianHistogram {
                         aux = hist[j]; //The median is the last pixel of its column
                     } else {
                         aux--; //The previous median wasn't the first pixel of its column, so it doesn't change, just need to mark its new position
-                    }
-                } else if (pixel2 == median) //new pixel = last median
-                {
-                    if (pixel < median) //old pixel < last median, the median goes right
-                    {
-                        aux++; //There is at least one pixel above the last median (the one that was just added), so the median doesn't change, just need to mark its new position
-                    }                                //else, absolutely nothing changes
-                } else //pixel==median, old pixel = last median
-                {
-                    if (pixel2 > median) //new pixel > last median, the median goes right
-                    {
-                        if (aux == (hist[median] + 1)) //The previous median was the last pixel of its column, so it changes
-                        {
-                            j++;
-                            while (hist[j] == 0) //Searching for the next pixel
-                            {
-                                j++;
-                            }
-                            median = (short) (j);
-                            aux = 1; //The median is the first pixel of its column
-                        }
-                        //else, absolutely nothing changes
-                    } else //pixel2<median, new pixel < last median, the median goes left
-                    {
-                        if (aux == 1) //The previous median was the first pixel of its column in the histogram, so it changes
-                        {
-                            j--;
-                            while (hist[j] == 0) //Searching for the next pixel
-                            {
-                                j--;
-                            }
-                            median = (short) (j);
-                            aux = hist[j]; //The median is the last pixel of its column
-                        } else {
-                            aux--; //The previous median wasn't the first pixel of its column, so it doesn't change, just need to mark its new position
-                        }
                     }
                 }
             }
